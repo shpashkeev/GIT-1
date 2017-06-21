bfac9c7aeebe66ebf4eac97d01cddedc6df7fa81
shpashkeev <sanchez.pashkeson@gmail.com>
2017-06-21 21:08:23 +0300

    DEV-1

--- DEV-1/Program.cs
+++ DEV-1/Program.cs
@@ -6,18 +6,18 @@ namespace PrintNumbers
     {
         static void Main(string[] args)
         {
-            String output;
+			String output;
             for (int i=0; i<=100; i++)
             {
                 if (i % 3 == 0)
-                {
-                    output = "3*" + (i / 3).ToString();
-                }
-                else
-                {
-                    output = i.ToString();
-                }
-                Console.WriteLine(output);
+				{
+					output = "3*" + (i / 3).ToString();
+				}
+				else
+				{
+					output = i.ToString();
+				}
+				Console.WriteLine(output);
             }
             Console.ReadKey();
         }
