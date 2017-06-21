ca17083da1e1fc142b4fec94b9926d925120a9de
shpashkeev <sanchez.pashkeson@gmail.com>
2017-06-21 18:22:35 +0300

    tutti-frutti changes

---
+++ PrintNumbers/PrintNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrintNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i<=100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Tutti-Frutti");
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Tutti");
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Frutti");
+                    continue;
+                }
+                Console.WriteLine(i.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
