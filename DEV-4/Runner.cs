f2e2f24bfdac3f5a72422db6653dc5f1cc46f8e5
shpashkeev <sanchez.pashkeson@gmail.com>
2017-06-26 16:00:14 +0300

    fix code style 2

--- DEV-4/Runner.cs
+++ DEV-4/Runner.cs
@@ -8,45 +8,45 @@ namespace CheckSequence
         {
             do
             {
-				try
-				{
-					// User read elements of sequence
-					Console.Write("Enter sequence with splits: ");
-					string[] stringSequence = Console.ReadLine().Split();
+                try
+                {
+                    // User read elements of sequence
+                    Console.Write("Enter sequence with splits: ");
+                    string[] stringSequence = Console.ReadLine().Split();
 
-					int sizeOfSequence = stringSequence.Length;
-					int[] sequence = new int[sizeOfSequence];
+                    int sizeOfSequence = stringSequence.Length;
+                    int[] sequence = new int[sizeOfSequence];
 
-					bool check = false;
+                    bool check = false;
 
-					for (int i = 0; i < sizeOfSequence; i++)
-					{
-						// Parse stringSequence element to int sequence
-						sequence[i] = int.Parse(stringSequence[i]);
-					}
+                    for (int i = 0; i < sizeOfSequence; i++)
+                    {
+                        // Parse stringSequence element to int sequence
+                        sequence[i] = int.Parse(stringSequence[i]);
+                    }
 
-					// Cycle for testing sequence on non-decreasing
-					for (int j = 0; j < (sizeOfSequence - 1); j++)
-					{
-						if (sequence[j + 1] < sequence[j])
-						{
-							check = true;
-							break;
-						}
-					}
-					if (check == true)
-					{
-						Console.WriteLine("This sequence is not non-decreasing");
-					}
-					else
-					{
-						Console.WriteLine("This sequence is non-decreasing");
-					}
-				}
-				catch(Exception exc)
-				{
-					Console.WriteLine("Exception: " + exc);
-				}
+                    // Cycle for testing sequence on non-decreasing
+                    for (int j = 0; j < (sizeOfSequence - 1); j++)
+                    {
+                        if (sequence[j + 1] < sequence[j])
+                        {
+                            check = true;
+                            break;
+                        }
+                    }
+                    if (check == true)
+                    {
+                        Console.WriteLine("This sequence is not non-decreasing");
+                    }
+                    else
+                    {
+                        Console.WriteLine("This sequence is non-decreasing");
+                    }
+                }
+                catch (Exception exc)
+                {
+                    Console.WriteLine("Exception: " + exc);
+                }
                 Console.WriteLine("Press Esc to exit, or any key to continue");
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
