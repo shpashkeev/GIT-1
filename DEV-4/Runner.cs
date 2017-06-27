3249d0274e4038f04eb8fee501e59c0018a071b8
shpashkeev <sanchez.pashkeson@gmail.com>
2017-06-27 23:47:18 +0300

    modify DEV-4

--- DEV-4/Runner.cs
+++ DEV-4/Runner.cs
@@ -2,58 +2,45 @@
 
 namespace CheckSequence
 {
-    class Runner
+  class Runner
+  {
+    const string ANSWER_NO = "This sequence is not non-decreasing";
+    const string ANSWER_YES = "This sequence is non-decreasing";
+    const string CHOICE = "\nPress Esc to exit, or any key to continue";
+
+    static void Main(string[] args)
     {
-        const string ENTER_LINE = "Enter sequence with splits: ";
-        const string ANSWER_NO = "This sequence is not non-decreasing";
-        const string ANSWER_YES = "This sequence is non-decreasing";
-        const string CHOICE = "Press Esc to exit, or any key to continue";
-
-        static void Main(string[] args)
+      do
+      {
+        try
         {
-            do
+          bool check = false;
+          int[] sequence = (new Inputer()).SequenceReturn(args);
+
+          // Cycle for testing sequence on non-decreasing
+          for (int j = 0; j < (sequence.Length - 1); j++)
+          {
+            if (sequence[j + 1] < sequence[j])
             {
-                try
-                {
-                    // User read elements of sequence
-                    Console.Write(ENTER_LINE);
-                    string[] stringSequence = Console.ReadLine().Split();
-
-                    int sizeOfSequence = stringSequence.Length;
-                    int[] sequence = new int[sizeOfSequence];
-
-                    bool check = false;
-
-                    for (int i = 0; i < sizeOfSequence; i++)
-                    {
-                        // Parse stringSequence element to int sequence
-                        sequence[i] = int.Parse(stringSequence[i]);
-                    }
-
-                    // Cycle for testing sequence on non-decreasing
-                    for (int j = 0; j < (sizeOfSequence - 1); j++)
-                    {
-                        if (sequence[j + 1] < sequence[j])
-                        {
-                            check = true;
-                            break;
-                        }
-                    }
-                    if (check == true)
-                    {
-                        Console.WriteLine(ANSWER_NO);
-                    }
-                    else
-                    {
-                        Console.WriteLine(ANSWER_YES);
-                    }
-                }
-                catch (Exception exc)
-                {
-                    Console.WriteLine("Exception: " + exc);
-                }
-                Console.WriteLine(CHOICE);
-            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+              check = true;
+              break;
+            }
+          }
+          if (check == true)
+          {
+            Console.WriteLine(ANSWER_NO);
+          }
+          else
+          {
+            Console.WriteLine(ANSWER_YES);
+          }
         }
+        catch (Exception exc)
+        {
+          Console.WriteLine("Exception: " + exc);
+        }
+        Console.WriteLine(CHOICE);
+      } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
     }
+  }
 }
