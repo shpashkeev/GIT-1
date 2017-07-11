26de744ed9d6dea1ba1588972cbdb5029c0647be
shpashkeev <sanchez.pashkeson@gmail.com>
2017-07-11 09:04:25 +0300

    added triangle type error exception

--- DEV-7/TriangleTypeDetector.cs
+++ DEV-7/TriangleTypeDetector.cs
@@ -7,36 +7,45 @@ namespace TypeOfTriangle
     private double sideA;
     private double sideB;
     private double sideC;
+
     const double EPSILON = 10e-6;
+    private Exception triangleTypeError = new Exception("Triangle type error");
 
     public TypeOfTriangles GetTriangleType(double[] sides)
     {
-      sideA = sides[0];
-      sideB = sides[1];
-      sideC = sides[2];
-
-      // Condition of existance
-      if (!(sideA + sideB >= sideC && sideB + sideC >= sideA && sideA + sideC >= sideB))
+      try
       {
-        throw new Exception();
-      }
+        sideA = sides[0];
+        sideB = sides[1];
+        sideC = sides[2];
 
-      // Condition for an equilateralor triangle
-      if (Math.Abs(sideA - sideB) < EPSILON && Math.Abs(sideB - sideC) < EPSILON)
+        // Condition of existance
+        if (!(sideA + sideB >= sideC && sideB + sideC >= sideA && sideA + sideC >= sideB))
+        {
+          throw triangleTypeError;
+        }
+
+        // Condition for an equilateralor triangle
+        if (Math.Abs(sideA - sideB) < EPSILON && Math.Abs(sideB - sideC) < EPSILON)
+        {
+          return TypeOfTriangles.Equilateralor;
+        }
+
+        // Condition for an isosceles triangle
+        if (Math.Abs(sideA - sideB) < EPSILON ||
+          Math.Abs(sideA - sideC) < EPSILON ||
+          Math.Abs(sideB - sideC) < EPSILON)
+        {
+          return TypeOfTriangles.Isosceles;
+        }
+
+        // Default type of triangle
+        return TypeOfTriangles.Usual;
+      }
+      catch
       {
-        return TypeOfTriangles.Equilateralor;
+        throw triangleTypeError;
       }
-
-      // Condition for an isosceles triangle
-      if (Math.Abs(sideA - sideB) < EPSILON ||
-        Math.Abs(sideA - sideC) < EPSILON ||
-        Math.Abs(sideB - sideC) < EPSILON)
-      {
-        return TypeOfTriangles.Isosceles;
-      }
-
-      // Default type of triangle
-      return TypeOfTriangles.Usual;
     }
   }
 }
