faf6ea109fc2176af62e2f0d46b25bfded9c3a29
shpashkeev <sanchez.pashkeson@gmail.com>
2017-07-25 13:20:34 +0300

    created test project

---
+++ TypeOfTriangle.Test/UsualTriangleBuilderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TypeOfTriangle.Test
+{
+  [TestClass]
+  public class UsualTriangleBuilderTest
+  {
+    [TestMethod]
+    public void Build_UsualTriangleForValidValuesOfSides_IsUsualTriangle()
+    {
+    }
+  }
+}
