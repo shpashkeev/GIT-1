66e1ff59524bfd7b96bac1723f9e351f4bdd7223
shpashkeev <sanchez.pashkeson@gmail.com>
2017-07-25 13:29:28 +0300

    added datasource with excel file

--- TypeOfTriangle.Test/UsualTriangleBuilderTest.cs
+++ TypeOfTriangle.Test/UsualTriangleBuilderTest.cs
@@ -6,6 +6,11 @@ namespace TypeOfTriangle.Test
   [TestClass]
   public class UsualTriangleBuilderTest
   {
+    public TestContext TestContext { get; set; }
+
+    private const string dataDriver = "System.Data.OleDb";
+    private const string connectionStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=ValidDataForTriangle.xlsx;Extended Properties=\"Excel 12.0 Xml;HDR=YES\";";
+
     [TestMethod]
     public void Build_UsualTriangleForValidValuesOfSides_IsUsualTriangle()
     {
