2badc4f14e76ad00bc19ab1fb65da4af8445bac1
shpashkeev <sanchez.pashkeson@gmail.com>
2017-08-11 19:04:06 +0300

    added DEV-8 project with classes EntryPoint and Product

---
+++ DEV-8/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseOfProducts
+{
+  class Product
+  {
+    public string Type { get; }
+    public string Name { get; }
+    public int Count { get; set; }
+    public double Price { get; set; }
+  }
+}
