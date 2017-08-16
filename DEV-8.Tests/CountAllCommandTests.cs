53b27f68dd3946e30ed7cbf1ae4093a68af3d0c7
shpashkeev <sanchez.pashkeson@gmail.com>
2017-08-16 13:33:14 +0300

    added test project with first tests

---
+++ DEV-8.Tests/CountAllCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductsStorage.Commands;
+
+namespace ProductsStorage.Tests
+{
+  [TestClass]
+  public class CountAllCommandTests
+  {
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Execute_CommandWithEmptyProductsList_ArgumentNullException()
+    {
+      // arrange
+      var countAllCommand = new CountAllCommand();
+      // act
+      countAllCommand.execute(new List<Product>());
+      // assert
+    }
+  }
+}
---
+++ DEV-8.Tests/ProductBuilderTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProductsStorage;
+
+namespace ProductsStorage.Tests
+{
+  [TestClass]
+  public class ProductBuilderTests
+  {
+    public TestContext TestContext { get; set; }
+    private const string TYPE = "type";
+    private const string NAME = "name";
+    private const int COUNT = 1;
+    private const double PRICE = 1.0;
+
+    private const string connectionStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\\;Extended Properties=\"Excel 12.0 Xml;HDR=YES\";";
+
+    [TestMethod]
+    [DataSource(connectionStr,"PositiveCounts")]
+    public void Build_ProductWithValidCountParameter_IsProduct()
+    {
+      // arrange
+      int count = Convert.ToInt32(TestContext.DataRow["Count"]);
+      // act
+      var res = new ProductBuilder().Build(TYPE, NAME, count, PRICE);
+      // assert
+      Assert.IsInstanceOfType(res,typeof(Product));
+    }
+
+    [TestMethod]
+    [DataSource(connectionStr, "PositivePrices")]
+    public void Build_ProductWithValidPriceParameter_IsProduct()
+    {
+      // arrange
+      double price = Convert.ToDouble(TestContext.DataRow["Price"]);
+      // act
+      var res = new ProductBuilder().Build(TYPE, NAME, COUNT, price);
+      // assert
+      Assert.IsInstanceOfType(res, typeof(Product));
+    }
+  }
+}
