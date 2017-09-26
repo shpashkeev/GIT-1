3882a24248231abaadef81f03e698d9a30c0a80c
shpashkeev <sanchez.pashkeson@gmail.com>
2017-09-26 20:02:28 +0300

    added replacer logic

--- DEV-9/Replacer.cs
+++ DEV-9/Replacer.cs
@@ -8,9 +8,21 @@ namespace ReplacementOfCharacterGroups
 {
   public class Replacer
   {
-    public void Replacement(string firstSequence, string secondSequnce)
+    public string Replacement(string firstSequence, string secondSequence)
     {
-      
+      Randomizer randomizer = new Randomizer();
+
+      int indexInFirstSequence = randomizer.RandomStartIndex(firstSequence);
+      int indexInSecondSequence = randomizer.RandomStartIndex(secondSequence);
+
+      int groupLengthFromFirstSequnce = randomizer.RandomCount(firstSequence, indexInFirstSequence);
+      int groupLengthFromSecondSequnce = randomizer.RandomCount(secondSequence, indexInSecondSequence);
+
+      string firstGroup = firstSequence.Substring(indexInFirstSequence, groupLengthFromSecondSequnce);
+      string secondGroup = secondSequence.Substring(indexInSecondSequence, groupLengthFromSecondSequnce);
+
+      string result = firstSequence.Replace(firstGroup, secondGroup);
+      return result;
     }
   }
 }
