7b77c5ead947409c3b35e431517826fc609d7656
shpashkeev <sanchez.pashkeson@gmail.com>
2017-09-26 20:49:24 +0300

    deleted unnecessary lines

--- DEV-9/EntryPoint.cs
+++ DEV-9/EntryPoint.cs
@@ -9,15 +9,12 @@ namespace ReplacementOfCharacterGroups
       string firstSequence = "abcdefghijklmnop";
       string secondSequence = "1234567890";
 
-      do
-      {
-        string thirdSequence = new Replacer().Replacement(firstSequence, secondSequence);
+      string thirdSequence = new Replacer().Replacement(firstSequence, secondSequence);
 
-        Console.WriteLine($"#1: {firstSequence}");
-        Console.WriteLine($"#2: {secondSequence}");
-        Console.WriteLine($"#3: {thirdSequence}");
+      Console.WriteLine($"#1: {firstSequence}");
+      Console.WriteLine($"#2: {secondSequence}");
+      Console.WriteLine($"#3: {thirdSequence}");
 
-      } while (Console.ReadKey(true).Key == ConsoleKey.Enter);
       Console.ReadKey();
     }
   }
