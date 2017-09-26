3087aad8a981996e4d443495ccc0d050899d0734
shpashkeev <sanchez.pashkeson@gmail.com>
2017-09-26 20:05:30 +0300

    added EntryPoint logic

--- DEV-9/EntryPoint.cs
+++ DEV-9/EntryPoint.cs
@@ -10,6 +10,16 @@ namespace ReplacementOfCharacterGroups
   {
     static void Main(string[] args)
     {
+      string firstSequence = "abcdefg";
+      string secondSequence = "1234567";
+
+      string thirdSequence = new Replacer().Replacement(firstSequence,secondSequence);
+
+      Console.WriteLine($"#1: {firstSequence}");
+      Console.WriteLine($"#2: {secondSequence}");
+      Console.WriteLine($"#3: {thirdSequence}");
+
+      Console.ReadKey();
     }
   }
 }
