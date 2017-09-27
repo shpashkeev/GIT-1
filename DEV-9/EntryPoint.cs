90a240feb0bdf1a5bc838a6a6b29df9208c63723
shpashkeev <sanchez.pashkeson@gmail.com>
2017-09-27 18:09:06 +0300

    added reading from file

--- DEV-9/EntryPoint.cs
+++ DEV-9/EntryPoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReplacementOfCharacterGroups
 {
@@ -6,16 +7,24 @@ namespace ReplacementOfCharacterGroups
   {
     static void Main(string[] args)
     {
-      string firstSequence = args[0];
-      string secondSequence = args[1];
+      try
+      {
+        List<string> seqList = new FileReader().ReadLines(@"c:\Text.txt");
+        string firstSequence = seqList[0];
+        string secondSequence = seqList[1];
 
-      string thirdSequence = new Replacer().Replacement(firstSequence, secondSequence);
+        string thirdSequence = new Replacer().Replacement(firstSequence, secondSequence);
 
-      Console.WriteLine($"#1: {firstSequence}");
-      Console.WriteLine($"#2: {secondSequence}");
-      Console.WriteLine($"#3: {thirdSequence}");
+        Console.WriteLine($"#1: {firstSequence}");
+        Console.WriteLine($"#2: {secondSequence}");
+        Console.WriteLine($"#3: {thirdSequence}");
 
-      Console.ReadKey();
+        Console.ReadKey();
+      }
+      catch (Exception exc)
+      {
+        Console.WriteLine(exc.Message);
+      }
     }
   }
 }
---
+++ DEV-9/FileReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ReplacementOfCharacterGroups
+{
+  public class FileReader
+  {
+    private const string FileReaderError = "File reader error";
+
+    public List<string> ReadLines(string path)
+    {
+      StreamReader file = new StreamReader(path);
+      List<string> result = new List<string>();
+      string line;
+      try
+      {
+        while ((line=file.ReadLine()) != null)
+        {
+          result.Add(line);
+        }
+        return result;
+      }
+      catch (Exception e)
+      {
+        throw new Exception(FileReaderError);
+      }
+    }
+  }
+}
