1c85d59c15ba71b85ae7fa7e5070d967e51523d3
shpashkeev <sanchez.pashkeson@gmail.com>
2017-09-27 13:29:31 +0300

    comments to methods of class Replacer

--- DEV-9/Replacer.cs
+++ DEV-9/Replacer.cs
@@ -7,12 +7,15 @@ namespace ReplacementOfCharacterGroups
     {
       Randomizer randomizer = new Randomizer();
 
+      // Obtaining random start indices of groops in sequences
       int indexInFirstSequence = randomizer.RandomStartIndex(firstSequence);
       int indexInSecondSequence = randomizer.RandomStartIndex(secondSequence);
 
+      // Obtaining random lengths of groups
       int groupLengthFromFirstSequnce = randomizer.RandomLength(firstSequence, indexInFirstSequence);
       int groupLengthFromSecondSequnce = randomizer.RandomLength(secondSequence, indexInSecondSequence);
 
+      // Selecting a groups from sequences
       string firstGroup = firstSequence.Substring(indexInFirstSequence, groupLengthFromFirstSequnce);
       string secondGroup = secondSequence.Substring(indexInSecondSequence, groupLengthFromSecondSequnce);
 
