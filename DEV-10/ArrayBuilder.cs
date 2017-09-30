8d8612caba2e6349ad0dfbf9155a5e0e1cbd8f03
shpashkeev <sanchez.pashkeson@gmail.com>
2017-10-01 01:14:46 +0300

    created DEV-10 project and added new array collecting

---
+++ DEV-10/ArrayBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectOneArrayFromN
+{
+  public class ArrayBuilder
+  {
+    public double[] BuildArrayDoubles(int size)
+    {
+      Random randGen = new Random();
+      if (size == 0)
+      {
+        throw new ArgumentNullException();
+      }
+      double[] result = new double[size];
+      for (int i = 0; i < size; i++)
+      {
+        result[i] = randGen.NextDouble();
+      }
+      return result;
+    }
+  }
+}
---
+++ DEV-10/ArrayCollector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectOneArrayFromN
+{
+  public class ArrayCollector
+  {
+    private const double Epsilon = 10E-6;
+    public double[] CollectArrayDoubles(List<double[]> arrays)
+    {
+      List<double> listDoubles = new List<double>();
+      int arraysCount = arrays.Count;
+
+      for (int i = 0; i < arraysCount; i++)
+      {
+        double[] firstArray = arrays[i];
+        for (int j = i; j < arraysCount; j++)
+        {
+          double[] secondArray = arrays[j];
+          foreach (var elem1 in firstArray)
+          {
+            foreach (var elem2 in secondArray)
+            {
+              if (Math.Abs(elem1 - elem2) <= Epsilon && !listDoubles.Contains(elem1) && !listDoubles.Contains(elem2))
+              {
+                listDoubles.Add(elem1);
+              }
+            }
+          }
+        }
+      }
+      int resultArrayLength = listDoubles.Count;
+      double[] result = new double[resultArrayLength];
+      for (int i = 0; i < resultArrayLength; i++)
+      {
+        result[i] = listDoubles[i];
+      }
+      return result;
+    }
+  }
+}
