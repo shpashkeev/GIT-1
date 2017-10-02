4e4e236027ed8f2c7c2b6ab95a1067794102aa0e
shpashkeev <sanchez.pashkeson@gmail.com>
2017-10-02 22:23:15 +0300

    added DEV-11 with class TextFileReader and Latin and Cyril Dictionaries

---
+++ DEV-11/TextFileReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ReplacementOfCharacterGroups
+{
+  // Class contains a method
+  // that reads lines from a text file.
+  public class TextFileReader
+  {
+    // The complete file path is specified by the path parameter.
+    // StreamReader class constructor initializes the encoding to UTF8Encoding.
+    public string ReadLines(string path)
+    {
+      using (StreamReader file = new StreamReader(path))
+      {
+        return file.ReadToEnd();
+      }
+    }
+  }
+}
