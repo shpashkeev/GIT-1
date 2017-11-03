3f8f2573f6d74c4e06781494b74747d242526513
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-04 01:38:34 +0300

    added third criterion

---
+++ DEV-13/Criterions/MinCountOfWorkers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StaffSelection.Fellow_Workers;
+
+namespace StaffSelection.Criterions
+{
+  public class MinCountOfWorkers : ICriterionSelectable
+  {
+    public List<FellowWorker> Select(double amount, int productivity)
+    {
+      double currentCost = 0.0;
+      int currentProductivity = 0;
+      List<FellowWorker> currentFellowWorkers = new List<FellowWorker>();
+
+      return currentFellowWorkers;
+    }
+  }
+}
