7dfde7ab1463e1817093a27fdac9c046a4949fd5
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-04 01:43:33 +0300

    added abstact fellow worker builder

---
+++ DEV-13/Fellow Workers Builders/FellowWorkerBuilder.cs
@@ -0,0 +1,11 @@
+﻿using StaffSelection.Fellow_Workers;
+
+namespace StaffSelection.Fellow_Workers_Builders
+{
+  abstract class FellowWorkerBuilder
+  {
+    protected FellowWorkerBuilder successor;
+
+    public abstract FellowWorker Build(double salary, int productivity);
+  }
+}
