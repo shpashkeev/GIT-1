fd8423c887e1ab20d87c25e0783f852245b4f135
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-04 01:53:38 +0300

    added junior builder

---
+++ DEV-13/Fellow Workers Builders/JuniorBuilder.cs
@@ -0,0 +1,28 @@
+﻿using StaffSelection.Fellow_Workers;
+
+namespace StaffSelection.Fellow_Workers_Builders
+{
+  class JuniorBuilder : FellowWorkerBuilder
+  {
+    public JuniorBuilder(FellowWorkerBuilder successor)
+    {
+      this.successor = successor;
+    }
+
+    public override FellowWorker Build(double salary, int productivity)
+    {
+      try
+      {
+        return new Junior(salary, productivity);
+      }
+      catch (FellowWorkerException)
+      {
+        if (successor != null)
+        {
+          return successor.Build(salary, productivity);
+        }
+        throw new FellowWorkerException();
+      }
+    }
+  }
+}
