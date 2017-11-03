2d5a5d7d68a0edaf54fbfd2f67431417f1195601
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-04 00:45:40 +0300

    added class selector with fields sum and productivity

---
+++ DEV-13/StaffSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffSelection
+{
+  public class StaffSelector
+  {
+    private double currentSum;
+    private int productivity;
+
+    public double CurrentSum
+    {
+      get { return currentSum; }
+      set
+      {
+        if (value <= 0)
+        {
+          throw new ArgumentException();
+        }
+        currentSum = value;
+      }
+    }
+
+    public int Productivity
+    {
+      get { return productivity; }
+      set { productivity = value; }
+    }
+  }
+}
