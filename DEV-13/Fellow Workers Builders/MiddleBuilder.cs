26781fca09f638220804c389f1bf9cf6f2749f06
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-04 01:57:01 +0300

    added middle builder

---
+++ DEV-13/Fellow Workers Builders/MiddleBuilder.cs
@@ -0,0 +1,27 @@
+﻿using StaffSelection.Fellow_Workers;
+
+namespace StaffSelection.Fellow_Workers_Builders
+{
+  class MiddleBuilder : FellowWorkerBuilder
+  {
+    public MiddleBuilder(FellowWorkerBuilder successor)
+    {
+      this.successor = successor;
+    }
+    public override FellowWorker Build(double salary, int productivity)
+    {
+      try
+      {
+        return new Middle(salary, productivity);
+      }
+      catch (FellowWorkerException)
+      {
+        if (successor != null)
+        {
+          return successor.Build(salary, productivity);
+        }
+        throw new FellowWorkerException();
+      }
+    }
+  }
+}
