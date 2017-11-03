e31fe0ae0add57f501f5f48823106b1d1a18ec26
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-04 01:08:53 +0300

    added senior

---
+++ DEV-13/Fellow Workers/Senior.cs
@@ -0,0 +1,18 @@
+﻿
+namespace StaffSelection.Fellow_Workers
+{
+  public class Senior : FellowWorker
+  {
+    public Senior(double salary, int productivity) : base(salary, productivity) { }
+
+    public override Qualifications GetQualification()
+    {
+      return Qualifications.Senior;
+    }
+
+    public override string GetQualificationString()
+    {
+      return Iam + GetQualification();
+    }
+  }
+}
