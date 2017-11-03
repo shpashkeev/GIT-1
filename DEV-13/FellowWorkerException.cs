0bdf04d636c74841559ecf4fecd722210d56861d
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-04 01:51:13 +0300

    added exception if qualification is incorrect

---
+++ DEV-13/FellowWorkerException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StaffSelection
+{
+  public class FellowWorkerException : Exception
+  {
+    private const string IncorrectedQualification = "Incorrect qualification";
+
+    public FellowWorkerException() : base(IncorrectedQualification) { }
+  }
+}
