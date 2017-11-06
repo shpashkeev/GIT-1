47174a5da0d68bf53b2e9d39cc021992036fbca9
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-06 16:17:54 +0300

    added class Team

---
+++ DEV-13/Team.cs
@@ -0,0 +1,21 @@
+﻿
+using StaffSelection.Fellow_Workers;
+
+namespace StaffSelection
+{
+  public class Team
+  {
+    public Junior Junior { get; set; }
+    public Middle Middle { get; set; }
+    public Senior Senior { get; set; }
+    public Lead Lead { get; set; }
+
+    public Team(Junior junior, Middle middle, Senior senior, Lead lead)
+    {
+      this.Junior = junior;
+      this.Middle = middle;
+      this.Senior = senior;
+      this.Lead = lead;
+    }
+  }
+}
