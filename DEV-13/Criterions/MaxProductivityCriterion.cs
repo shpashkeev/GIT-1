b1468569ee038c7c9bab40ccd07b24235ebdb0f2
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-07 18:54:06 +0300

    added criterion 1/ comments

--- DEV-13/Criterions/MaxProductivityCriterion.cs
+++ DEV-13/Criterions/MaxProductivityCriterion.cs
@@ -5,8 +5,17 @@ using StaffSelection.FellowWorkers;
 
 namespace StaffSelection.Criterions
 {
+  /// <summary>
+  /// Criterion
+  /// 1/ Maximum productivity within the amount.
+  /// </summary>
   public class MaxProductivityCriterion : ICriterionSelectable
   {
+    /// <summary>
+    /// The problem of integer programming:
+    /// Selection employees necessary to maximize productivity within the cash amount.
+    /// </summary>
+    /// <param name="selector">Contains cash amount and productivity</param>
     public void Select(StaffSelector selector)
     {
       SolverContext context = SolverContext.GetContext();
@@ -18,24 +27,21 @@ namespace StaffSelection.Criterions
       Senior senior = selector.Staffs.Senior;
       Lead lead = selector.Staffs.Lead;
 
+      // init decisions - counts of number of employees of different qualifications
       Decision juniorDecision = new Decision(Domain.IntegerNonnegative, junior.GetQualificationString());
       Decision middleDecision = new Decision(Domain.IntegerNonnegative, middle.GetQualificationString());
       Decision seniorDecision = new Decision(Domain.IntegerNonnegative, senior.GetQualificationString());
       Decision leadDecision = new Decision(Domain.IntegerNonnegative, lead.GetQualificationString());
       model.AddDecisions(juniorDecision, middleDecision, seniorDecision, leadDecision);
 
-      model.AddConstraints("limits",
-        0 <= juniorDecision,
-        0 <= middleDecision,
-        0 <= seniorDecision,
-        0 <= leadDecision);
-
+      // constraint of max cash amount
       model.AddConstraints("maxCost",
        junior.Salary * juniorDecision + 
        middle.Salary * middleDecision + 
        senior.Salary * seniorDecision +
        lead.Salary * leadDecision <= selector.Amount);
 
+      // criterion of optimization - maximize total productivity
       model.AddGoal("productivity", GoalKind.Maximize,
         junior.Productivity * juniorDecision +
         middle.Productivity * middleDecision +
@@ -43,9 +49,12 @@ namespace StaffSelection.Criterions
         lead.Productivity * leadDecision);
 
       Solution solution = context.Solve(new ConstraintProgrammingDirective());
+
+      // print results
+      int numberOfSolution = 0;
       while (solution.Quality != SolverQuality.Infeasible)
       {
-        StringBuilder sb = new StringBuilder("Solution\n");
+        StringBuilder sb = new StringBuilder($"Solution #{++numberOfSolution}\n");
         sb.Append($"{junior.GetQualificationString()}: {juniorDecision} ")
           .Append($"{middle.GetQualificationString()}: {middleDecision} ")
           .Append($"{senior.GetQualificationString()}: {seniorDecision} ")
