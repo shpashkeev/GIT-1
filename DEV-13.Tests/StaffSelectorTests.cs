0e59efba51e0093f9edcb2b9156d840eb33eb9bd
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-08 12:51:11 +0300

    added test class StaffSelectorTests with simple test methods

---
+++ DEV-13.Tests/StaffSelectorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StaffSelection.Tests
+{
+  [TestClass]
+  public class StaffSelectorTests
+  {
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void InitSelector_FromInvalidParams_ArgumentException()
+    {
+      // arrange
+
+      // act
+    }
+
+    [TestMethod]
+    public void InitSelector_FromValidParams_IsStaffSelector()
+    {
+      // arrange
+      // act
+      // assert
+    }
+  }
+}
