d5660ba3a1aaa223f41d066d13185056aac6ef03
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-08 13:16:32 +0300

    added test class SeniorTests with simple test methods

---
+++ DEV-13.Tests/FellowWorkers.Tests/SeniorTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StaffSelection.Tests.FellowWorkers.Tests
+{
+  [TestClass]
+  public class SeniorTests
+  {
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Create_SeniorWithInvalidValues_ArgumentException()
+    {
+      // arrange
+      // act
+
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(FellowWorkerException))]
+    public void Create_SeniorWithInvalidParams_FellowWorkerException()
+    {
+      // arrange
+      // act
+    }
+
+    [TestMethod]
+    public void Create_SeniorWithValidParams_IsSenior()
+    {
+      // arrange
+      // act
+      // assert
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Set_SeniorInvalidSalary_ArgumentException()
+    {
+      // arrange
+      // act
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Set_SeniorInvalidProductivity_ArgumentException()
+    {
+      // arrange
+      // act
+    }
+  }
+}
