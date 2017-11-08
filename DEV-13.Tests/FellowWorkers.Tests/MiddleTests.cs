a8144216fab9be769186e74e20a0b35e857e3758
shpashkeev <sanchez.pashkeson@gmail.com>
2017-11-08 13:12:34 +0300

    added test class MiddleTests with simple test methods

---
+++ DEV-13.Tests/FellowWorkers.Tests/MiddleTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StaffSelection.Tests.FellowWorkers.Tests
+{
+  [TestClass]
+  public class MiddleTests
+  {
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Create_MiddleWithInvalidValues_ArgumentException()
+    {
+      // arrange
+      // act
+
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(FellowWorkerException))]
+    public void Create_MiddleWithInvalidParams_FellowWorkerException()
+    {
+      // arrange
+      // act
+    }
+
+    [TestMethod]
+    public void Create_MiddleWithValidParams_IsMiddle()
+    {
+      // arrange
+      // act
+      // assert
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Set_MiddleInvalidSalary_ArgumentException()
+    {
+      // arrange
+      // act
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void Set_MiddleInvalidProductivity_ArgumentException()
+    {
+      // arrange
+      // act
+    }
+  }
+}
